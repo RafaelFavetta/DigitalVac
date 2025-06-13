df9a5caadcd381157214f896606c5193d1b037e0
RafaelFavetta <rafaelfavetta@gmail.com>
2025-06-13 10:40:08 -0300

    correção do css

--- usuario/proxima_vac.php
+++ usuario/proxima_vac.php
@@ -353,6 +353,8 @@ foreach ($vacinas as $vacina) {
     <link rel="icon" href="../img/logo.png" type="image/png">
     <link rel="stylesheet" href="../bootstrap/bootstrap-5.3.6-dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
         .table {
             background: white;
             border-radius: 10px;
